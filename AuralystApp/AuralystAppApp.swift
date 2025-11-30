199f1cec011c7725cb307328144fb090d9fbaeee
Ryan Williams <ryan@ryanleewilliams.com>
2025-11-30 11:56:35 -0500

    Initial Commit

---
+++ AuralystApp/AuralystAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuralystAppApp.swift
+//  AuralystApp
+//
+//  Created by Ryan Williams on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AuralystAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuralystAppTests/AuralystAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuralystAppTests.swift
+//  AuralystAppTests
+//
+//  Created by Ryan Williams on 11/30/25.
+//
+
+import Testing
+
+struct AuralystAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
